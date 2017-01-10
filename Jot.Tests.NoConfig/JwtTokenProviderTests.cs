033f982c33b2ad7be80c55b548260bdc7d63fcbc
Brett Veenstra <brettveenstra@gmail.com>
2017-01-10 10:03:44 -0500

    basic test coverage for classes going to refactor

---
+++ Jot.Tests.NoConfig/JwtTokenProviderTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jot.Tests.NoConfig
+{
+    [TestClass]
+    public class JwtTokenProviderTests
+    {
+    }
+}
---
+++ Jot.Tests.NoConfig/JwtTokenTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Jot.Tests.NoConfig
+{
+    [TestClass]
+    public class JwtTokenTests
+    {
+        [TestMethod]
+        public void Create_NoTimeout_IssuedAtNotBeforeEqualsExpiration()
+        {
+            // Act
+            var sut = new JwtTokenProvider.JwtToken(0);
+
+            // Assert
+            Assert.IsNotNull(sut);
+
+            var issuedAt = sut.GetClaim<int>("iat");
+            var expires = sut.GetClaim<int>("exp");
+            var notBefore = sut.GetClaim<int>("nbf");
+
+            issuedAt.ShouldNotBe(0);
+            issuedAt.ShouldBe(notBefore);
+            expires.ShouldBe(issuedAt);
+        }
+
+        [TestMethod]
+        public void Create_WithTimeout_IssuedAtNotBeforeEqualsAndLessThanExpiration()
+        {
+            // Act
+            var sut = new JwtTokenProvider.JwtToken(10);
+
+            // Assert
+            Assert.IsNotNull(sut);
+
+            var issuedAt = sut.GetClaim<int>("iat");
+            var expires = sut.GetClaim<int>("exp");
+            var notBefore = sut.GetClaim<int>("nbf");
+
+            issuedAt.ShouldNotBe(0);
+            issuedAt.ShouldBe(notBefore);
+
+            expires.ShouldBeGreaterThan(issuedAt);
+        }
+    }
+}
